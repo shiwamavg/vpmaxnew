6f639976153d29073748294e91f64b05f9e2ffa5
shiwa <pdshiwam@gmail.com>
2026-08-30 15:07:06 +0530

    Add twitter:card meta tags for SEO

--- application/modules/template/views/header.php
+++ application/modules/template/views/header.php
@@ -25,6 +25,11 @@
   <meta property="og:image" content="<?= $img ?>"/>
   <meta property="og:url" content="<?= @$url ?>"/>
   <meta property="og:description" content="<?= @$description ?>"/>
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:title" content="<?= @$title ?>"/>
+  <meta name="twitter:description" content="<?= @$description ?>"/>
+  <meta name="twitter:image" content="<?= $img ?>"/>
+  <meta name="twitter:domain" content="<?= $company3 ?>"/>
   <meta name="robots" content="index, follow"/>
   <meta property="al:web:url" content="<?= $url ?>">
   <meta name="theme-color" content="<?= $themeColor ?>">
